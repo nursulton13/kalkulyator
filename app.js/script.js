fa9548f48bfc502f39a683f4f104eb9545ceedec
nursulton13 <nursultonahmadov65@gmail.com>
2021-12-22 13:03:11 +0500

    kalkulyator yangilandi

--- app.js/script.js
+++ app.js/script.js
@@ -4,11 +4,33 @@ document.querySelectorAll('td').forEach((td) => {
     td.addEventListener('click', (e) =>{
         var belgi = e.target.innerText;
 
-        if (belgi == "C") {
+        if(ekran.value == ''){
+            ekran.value += belgi;
+            obj = ekran.value.split("")
+            
+        } else if (belgi == "+" || belgi == "-" || belgi == "*" || belgi == "/") {
+            ekran.value += belgi;
+            obj = ekran.value.split("")
+            if (belgi == "+" || belgi == "-" || belgi == "*" || belgi == "/") {
+                for(i = 1; i < obj.length; i++){
+                    if (obj[i] == obj[i-1]) {
+                        belgi = obj.pop()
+                        ekran.value = obj.join('')
+                    }
+                }
+            }
+        } else if (belgi == "Ce") {
+            obj = ekran.value.split('');
+            belgi = obj.pop()
+            ekran.value = obj.join('')
+        } else if(belgi == "M+" || belgi == "M-" || belgi == "M"){
+            obj = ekran.value.split('');
+            ekran.value = obj.join('')
+        } else if (belgi == "C") {
             ekran.value = '';
         } else if(belgi == "="){
-            ekran.value = eval(ekran.value);
-        } else if(belgi == 'x2'){
+            ekran.value = eval(ekran.value);            
+        } else if(belgi == "^"){
             a = eval(ekran.value); 
             ekran.value = Math.pow(a, 2);
         } else if(belgi == ''){
@@ -17,6 +39,5 @@ document.querySelectorAll('td').forEach((td) => {
         } else {
             ekran.value += belgi;
         }
-
     })
 });
